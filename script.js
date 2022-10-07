697748d0fba8e5a6cbbeef839f0b82f52da58464
fabio vitharana <fabio17.vitharana@gmail.com>
2022-10-07 20:31:44 +0200

    project files added

---
+++ script.js
@@ -0,0 +1,6 @@
+//load event listener will make sure all the resources are loaded beforehand so there won't be any lagging
+window.addEventListener('load',() =>{
+    const canvas = document.getElementById('canvas-1');
+    const ctx = canvas.getContext('2d');
+
+}); 
