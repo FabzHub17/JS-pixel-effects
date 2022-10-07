3524a0b961e0c2b70d70468664fbe9469e45c89f
fabio vitharana <fabio17.vitharana@gmail.com>
2022-10-07 20:37:15 +0200

    classes added to create pixel particles and animate them

--- script.js
+++ script.js
@@ -3,4 +3,24 @@ window.addEventListener('load',() =>{
     const canvas = document.getElementById('canvas-1');
     const ctx = canvas.getContext('2d');
 
+    class Particle{
+        //blueprint to create individual particles
+        constructor(){
+            this.x = 0;
+            this.y = 0;
+            this.size = 3;
+        }
+
+    }
+
+    class Effect{
+        //handles all particles at the same time
+
+    }
+
+    const animate = () =>{
+        // animation loop to make everything animated and interactive
+
+    }
+
 }); 
